9824f342f8e38c990256efd90dcdea894b23be0c
zhmak <zhmakmaria@gmail.com>
2024-05-30 18:39:01 -0400

    Arrays

---
+++ PASV_Java_HW/src/Lesson4/Homework/Task10.java
@@ -0,0 +1,27 @@
+package Lesson4.Homework;
+
+import java.util.Scanner;
+
+public class Task10 {
+
+    /*
+    Посчитать факториал числа в границах от 10 до 15 (не используя рекурсию).
+     */
+    public static void main(String[] args) {
+
+        int n = (int) (Math.random() * 100);
+        int sum = 0;
+
+        for (int i = 0; i < n; i++) {   // i служит для перебора целых чисел
+            sum = sum + n;
+            if (n % 3 == 0);
+
+                System.out.println("сумма числа: " + n + " = " + sum);
+
+
+            }
+        }
+
+
+    }
+
