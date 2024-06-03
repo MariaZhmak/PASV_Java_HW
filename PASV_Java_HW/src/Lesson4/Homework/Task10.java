43c23d5224a30fb7ba708e3c1ca88cdc083ee7e8
zhmak <zhmakmaria@gmail.com>
2024-06-03 15:43:24 -0400

    Add Homework, Task 10 and 12

---
+++ PASV_Java_HW/src/Lesson4/Homework/Task10.java
@@ -0,0 +1,31 @@
+package Lesson4.Homework;
+
+import java.util.Scanner;
+
+public class Task10 {
+    /*
+    Посчитать факториал числа в границах от 10 до 15 (не используя рекурсию).
+     */
+
+    public static void main(String[] args) {
+
+        int num = 10;
+        int factorial = 1;
+
+        for (int i = 1; i <= num; i++) {
+            factorial *= i;
+
+        }
+
+        System.out.println("Factorial " + num + " = " + factorial);
+        while (num < 15) {
+            num++;
+            factorial = factorial * num;
+
+            System.out.println("Factorail " + num + " = " + factorial);
+        }
+
+    }
+
+
+}
