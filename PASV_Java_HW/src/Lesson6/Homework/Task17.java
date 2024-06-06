6185297cf777b30ed51282a722b9a2a141e5104c
zhmak <zhmakmaria@gmail.com>
2024-06-05 20:36:07 -0400

    Add Homework, Task17, Lesson6

---
+++ PASV_Java_HW/src/Lesson6/Homework/Task17.java
@@ -0,0 +1,43 @@
+package Lesson6.Homework;
+
+import java.util.Arrays;
+
+public class Task17 {
+    /*
+    Создать двухмерный квадратный массив, и заполнить его «бабочкой», т.е таким образом:
+    1 1 1 1 1
+    0 1 1 1 0
+    0 0 1 0 0
+    0 1 1 1 0
+    1 1 1 1 1
+     */
+
+    public static void main(String[] args) {
+
+       int matrix[][] = new int[5][5];
+
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                matrix[i][j] = 1;
+            }
+        }
+
+                for (int[] ints : matrix) {
+                    System.out.println();
+                    for (int anInt : ints) {
+
+                        matrix[1][0] = 0;
+                        matrix[1][4] = 0;
+                        matrix[2][0] = 0;
+                        matrix[2][1] = 0;
+                        matrix[2][3] = 0;
+                        matrix[2][4] = 0;
+                        matrix[3][0] = 0;
+                        matrix[3][4] = 0;
+
+                        System.out.print(anInt + " ");
+
+                    }
+                }
+            }
+        }
