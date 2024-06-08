cac108bb664feaaae90e9fcf35ca8defd15f86b9
zhmak <zhmakmaria@gmail.com>
2024-06-07 20:23:37 -0400

    Edit Task13

--- PASV_Java_HW/src/Lesson5/Homework/Task13.java
+++ PASV_Java_HW/src/Lesson5/Homework/Task13.java
@@ -9,16 +9,13 @@ public class Task13 {
 
     public static void main(String[] args) {
 
-        int min = 50;
-        int max = 100;
+        int[] nums = new int[10];
+        for (int i = 0; i < nums.length; i++) {
+            nums[i] = (int)(Math.random() * 50);
 
-        int random_int1 = (int)Math.floor(Math.random() * (max - min + 1) + min);
-        int random_int2 = (int)Math.floor(Math.random() * (max - min + 1) + min);
-        int random_int3 = (int)Math.floor(Math.random() * (max - min + 1) + min);
-
-        System.out.println("Sequence of random numbers:\n " + random_int1 + ", " + random_int2 + ", " + random_int3);
-
-        int nums[] = {random_int2, random_int2, random_int3};
+            System.out.print(nums[i] + ", ");
+        }
+        System.out.println();
 
         int length = nums.length;
         int highestNum = nums [0];
@@ -27,10 +24,7 @@ public class Task13 {
                 highestNum = num;
             }
         }
-        System.out.println("\nThe highest number in the array is: " + highestNum);
-
+        System.out.println("The highest number in the array is: " + highestNum);
 
     }
-
-
 }
