902bb82839c7b3de8d98430af3ca52505807e0fb
zhmak <zhmakmaria@gmail.com>
2024-06-07 20:56:07 -0400

    Edit Homework, Task17, Lesson6

--- PASV_Java_HW/src/Lesson6/Homework/Task17.java
+++ PASV_Java_HW/src/Lesson6/Homework/Task17.java
@@ -14,30 +14,47 @@ public class Task17 {
 
     public static void main(String[] args) {
 
-       int matrix[][] = new int[5][5];
+        int matrix[][] = new int[5][5];
 
         for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix[i].length; j++) {
+            for (int j = 0; j < matrix.length; j++) {
                 matrix[i][j] = 1;
+
+                matrix[1][0] = 0;
+                matrix[1][4] = 0;
+                matrix[2][0] = 0;
+                matrix[2][1] = 0;
+                matrix[2][3] = 0;
+                matrix[2][4] = 0;
+                matrix[3][0] = 0;
+                matrix[3][4] = 0;
+
+                System.out.printf("%d ", matrix[i][j]);
+
             }
+            System.out.println();
         }
 
-                for (int[] ints : matrix) {
-                    System.out.println();
-                    for (int anInt : ints) {
+        // Второй способ
 
-                        matrix[1][0] = 0;
-                        matrix[1][4] = 0;
-                        matrix[2][0] = 0;
-                        matrix[2][1] = 0;
-                        matrix[2][3] = 0;
-                        matrix[2][4] = 0;
-                        matrix[3][0] = 0;
-                        matrix[3][4] = 0;
-
-                        System.out.print(anInt + " ");
-
-                    }
-                }
-            }
-        }
+//                for (int i = 0; i < matrix.length; i++) {
+//            for (int j = 0; j < matrix[i].length; j++) {
+//                matrix[i][j] = 1;
+//            }
+//        }
+////                for (int[] ints : matrix) {
+//                    System.out.println();
+//                    for (int anInt : ints) {
+//
+//                        matrix[1][0] = 0;
+//                        matrix[1][4] = 0;
+//                        matrix[2][0] = 0;
+//                        matrix[2][1] = 0;
+//                        matrix[2][3] = 0;
+//                        matrix[2][4] = 0;
+//                        matrix[3][0] = 0;
+//                        matrix[3][4] = 0;
+//
+//                        System.out.print(anInt + " ");
+    }
+}
